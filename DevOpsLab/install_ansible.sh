566749a0a8498428da4607477f5c798aca7bcd23
Jeffrey Sykes <jeff.sykes@vmlyr.com>
2020-08-11 15:11:46 -0500

    Updated to use get-pip.py

--- DevOpsLab/install_ansible.sh
+++ DevOpsLab/install_ansible.sh
@@ -4,13 +4,17 @@
 localectl set-locale LANG=en_US.UTF-8
 
 ## Update and upgrade the packages
-#sudo yum update -y
+sudo yum update -y
 
 ## Install pre-requisite packages
-#sudo yum install -y openssl-devel libffi-devel python-devel python-pip
+sudo yum install -y openssl-devel libffi-devel python2-devel 
+
+## Install pip
+sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
 
 ## Install Ansible and Azure SDK via pip
-#sudo pip install ansible[azure]
+sudo pip install ansible[azure]
 
 ## Create a credentials file to store azure credentials
 ## sudo mkdir ~/.azure
